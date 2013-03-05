50dc7e9d239f188a21d1f158f90135b047522ee4
Eduardo Maia <eduvimaia@gmail.com>
2013-03-04 22:47:05 -0300

    configured haml as default template engine

--- lib/engine_on_rails/engine.rb
+++ lib/engine_on_rails/engine.rb
@@ -3,5 +3,9 @@ require 'bootstrap-sass'
 module EngineOnRails
   class Engine < ::Rails::Engine
     isolate_namespace EngineOnRails
+
+    config.generators do |g|
+      g.template_engine :haml
+    end
   end
 end
